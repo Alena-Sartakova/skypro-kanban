a510a456d38b933b34a3f2e145d1f79ba29ffb12
здесь ваше имя <  >
2024-04-17 17:07:46 +0700

    for

--- vite-project/src/components/popups/Exit/Exit.jsx
+++ vite-project/src/components/popups/Exit/Exit.jsx
@@ -5,12 +5,12 @@ const ExitPopup = () => {
                 <div className="pop-exit__ttl">
                     <h2>Выйти из аккаунта?</h2>
                 </div>
-                <htmlform className="pop-exit__htmlform" id="htmlformExit" action="#">
-                    <div className="pop-exit__htmlform-group">
+                <form className="pop-exit__form" id="formExit" action="#">
+                    <div className="pop-exit__form-group">
                         <button className="pop-exit__exit-yes _hover01" id="exitYes"><a href="modal/signin.html">Да, выйти</a> </button>
                         <button className="pop-exit__exit-no _hover03" id="exitNo"><a href="main.html">Нет, остаться</a> </button>
                     </div>
-                </htmlform>
+                </form>
             </div>
         </div>
     </div>);
